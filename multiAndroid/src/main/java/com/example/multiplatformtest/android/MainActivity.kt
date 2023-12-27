2dcbbcca85b050b0658899e0c9cdb2616f937dec
ksh1710 <kshitizagarwal1710@gmail.com>
2023-12-27 17:25:44 +0530

    first model and a little app UI

--- multiAndroid/src/main/java/com/example/multiplatformtest/android/MainActivity.kt
+++ multiAndroid/src/main/java/com/example/multiplatformtest/android/MainActivity.kt
@@ -3,39 +3,23 @@ package com.example.multiplatformtest.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.multiplatformtest.Greeting
+import com.example.multiplatformtest.android.app.PoliceApp
+import com.example.multiplatformtest.android.screens.SignUpScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    GreetingView("hello world")
-                }
-            }
+//            MyApplicationTheme {
+//                PoliceApp()
+            SignUpScreen()
+//            }
         }
     }
 }
 
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-//
-//@Preview
 //@Composable
-//fun DefaultPreview() {
-//    MyApplicationTheme {
-//        GreetingView("hello world :)")
-//    }
+//fun GreetingView(text: String) {
+//    Text(text = text)
 //}
